e70f5fb84ba32787432154b8336ef2ecf87609c6
John McCormick <jam@neardark.com>
2016-07-28 11:00:22 -0600

    Clean up user profile pages

--- profiles/dcco/themes/dcco2016/templates/user-profile.tpl.php
+++ profiles/dcco/themes/dcco2016/templates/user-profile.tpl.php
@@ -42,7 +42,9 @@
     print '<ul>';
       print '<li><label>Name</label>' . render($profile['field_first_name']) . ' ' . render($profile['field_last_name']) . '</li>';
       print '<li><label>Organization</label>' . render($profile['field_organization']);
+      print '<li><label>Job title</label>' . render($profile['field_job_title']);
       print '<li><label>Bio</label>' . render($profile['field_bio']);
+      print '<li><label>Drupal experience</label>' . render($profile['field_how_long_drupal']);
       print '<li><label>Twitter</label>' . render($profile['field_twitter']);
       print '<li><label>IRC</label>' . render($profile['field_irc']);
     print '</ul>';
